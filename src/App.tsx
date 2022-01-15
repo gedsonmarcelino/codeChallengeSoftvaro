0e12bee68a442a325b698cfc4287020f6a4cdb18
Gedson Marcelino <gedson.marcelino@gmail.com>
2022-01-15 17:11:25 -0300

    [added] browser router in all application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'
 
 function App() {
 	return (
-		<>
+		<Router>
 			<LandingPage />
-		</>
+		</Router>
 	);
 }
 
