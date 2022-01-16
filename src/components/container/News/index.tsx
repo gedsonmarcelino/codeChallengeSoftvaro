a40f38e238b8b1beae8f0632c740f5fb0715351d
Gedson Marcelino <gedson.marcelino@gmail.com>
2022-01-16 14:22:04 -0300

    [added] skeleton on news loading

--- src/components/container/News/index.tsx
+++ src/components/container/News/index.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import { Article, loadNews } from '../../../services/news';
 import { formatDate } from '../../../utils/format';
 
+import 'react-loading-skeleton/dist/skeleton.css'
 import './styles.scss'
+
+const NEWS_AMOUNT = 3;
 
 function News() {
 
@@ -14,7 +18,7 @@ function News() {
 	async function loadData() {
 		try {
 			setLoading(true)
-			const data = await loadNews()
+			const data = await loadNews(NEWS_AMOUNT)
 			setNews(data)
 		} catch (error) {
 			console.log('[LOAD NEWS - ERROR]', error);
@@ -28,7 +32,24 @@ function News() {
 		loadData()
 	}, [])
 
-	if (loading) return <p>Loading...</p>
+	// --- Renders ---
+	function renderLoading() {
+		console.log('[renderLoading]', [new Array(NEWS_AMOUNT)]);
+		const arr = Array.from({ length: NEWS_AMOUNT }, (v, k) => k + 1)
+		return (
+			<SkeletonTheme>
+				{arr.map(item => (
+					<div key={`loading-${item}`} className="news-item" >
+						<Skeleton width={160} height={160} />
+						<div className="news-item-data">
+							<Skeleton height={50} style={{ marginBottom: '10px' }} />
+							<Skeleton count={3} height={25} style={{ margin: '5px 0' }} />
+						</div>
+					</div>
+				))}
+			</SkeletonTheme>
+		)
+	}
 
 	return (
 		<section id='news'>
@@ -36,27 +57,34 @@ function News() {
 			<div className='news-content'>
 				<h2>Latest News</h2>
 
+
 				<div className="news-list">
-					{news.map(item => {
-						const date = new Date(item.publishedAt)
-						const key = date.getTime()
-						const dateFormated = formatDate(date)
-						return (
-							<a key={key} className="news-item" href={item.url} target="_blank" rel="noreferrer">
-								<img src={item.urlToImage} alt={item.title} />
-								<div className="news-item-data">
-									<h3>
-										{item.title}
-										<span>{dateFormated}</span>
-									</h3>
-									<p>{item.description}</p>
-								</div>
-							</a>
-						)
-					})}
+					{loading
+						? renderLoading()
+						:
+						<>
+							{news.map(item => {
+								const date = new Date(item.publishedAt)
+								const key = date.getTime()
+								const dateFormated = formatDate(date)
+								return (
+									<a key={key} className="news-item" href={item.url} target="_blank" rel="noreferrer">
+										<img src={item.urlToImage} alt={item.title} />
+										<div className="news-item-data">
+											<h3>
+												{item.title}
+												<span>{dateFormated}</span>
+											</h3>
+											<p>{item.description}</p>
+										</div>
+									</a>
+								)
+							})}
+						</>
+					}
 				</div>
 			</div>
-		</section>
+		</section >
 	);
 }
 export default News;
